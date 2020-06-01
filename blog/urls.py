67380db826cc7c0d5c2054aa8b6bbbd2646272e4
Eiji Tatsuta <spoon32@theia.ocn.ne.jp>
2020-06-01 15:25:02 +0900

    Added views to create edit blog post inside the site

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,19 @@ from . import views
 urlpatterns = [
     path('',views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
 ]
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
